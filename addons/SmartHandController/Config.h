363d00ded1253f5c4b893d78083bb87f4a620009
Lukas Tönne <lutnne@microsoft.com>
2020-10-09 14:32:08 +0200

    Config for smart hand controller.

--- addons/SmartHandController/Config.h
+++ addons/SmartHandController/Config.h
@@ -15,7 +15,7 @@
 //      Parameter Name              Value   Default  Notes                                                                      Hint
 // DISPLAY -------------------------------------------------------------------------------------------------------------------------
 #define DISPLAY_LANGUAGE             L_en //   L_en, English. Specify language with two letter country code, if supported.    Adjust
-#define DISPLAY_OLED               SH1106 // SH1106, OLED 1.3" I2C display commonly used. SSD1306 is a 0.96" OLED display.    Infreq
+#define DISPLAY_OLED               SSD1306 // SH1106, OLED 1.3" I2C display commonly used. SSD1306 is a 0.96" OLED display.    Infreq
                                           //         SSD1309 is a 2.3" I2C display (w/Reset on ESP32 Pin23 or Teensy Pin14.)
 
 // SERIAL PORTS --------------------------------------------------------------------------------------------------------------------
